8cc6d8389689b8a18f0dbca08fc6f236499422f8
Milena Kowalska <milenakowalska126@gmail.com>
2025-12-06 16:58:41 +0100

    allow origins

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*", "https://the-new-hires-frontend.vercel.app"]
     allow_origins=[origin.strip() for origin in os.getenv("FRONTEND_URL", "http://localhost:5173").split(",")], # Specific origin for credentials support
     allow_credentials=True,
     allow_methods=["*"],
